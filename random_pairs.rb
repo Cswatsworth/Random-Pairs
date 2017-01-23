b578b1a31976c9d52d0e426f0309ec98d989962d
Chad Swatsworth <Chad Swatsworth>
2017-01-23 16:11:26 -0500

    4,6,7,8 random pairs

--- random_pairs.rb
+++ random_pairs.rb
@@ -1,1 +1,16 @@
-random_pairs.rb
+def create_pairs(names) #array of strings
+	array = [] #make empty array
+	array_paired_names = names.shuffle.each_slice(2)
+	
+	array_paired_names.each do |pairs|
+		if pairs.length == 2
+			array << pairs
+		else
+			array.last << pairs
+		end
+
+	end
+	array 
+end
+
+#create_pairs(['Dan','Marvin','Tim','Shirley']))
--- test_random_pairs.rb
+++ test_random_pairs.rb
@@ -1,1 +1,32 @@
-test_random_pairs.rb
+require 'minitest/autorun'
+require_relative 'random_pairs.rb'
+
+class TestPairsArray < Minitest::Test
+
+	def test_two_names_returns_two_pairs
+		pairs = create_pairs(['Dan','Marvin'])
+		assert_equal(1, pairs.count)
+	end
+
+	def test_four_names_is_two_pairs
+		pairs = create_pairs(['Dan','Marvin','Tim','Shirley'])
+		assert_equal(2, pairs.count)
+	end
+
+	def test_six_names_returns_3_pairs
+		pairs = create_pairs(['Dan','Marvin','Tim','Shirley','Chloe','April'])
+		assert_equal(3, pairs.count)
+		
+	end
+
+	def test_7_names_returns_3_odd
+		pairs = create_pairs(['Dan','Marvin','Tim','Shirley','Chloe','April','Dan'])
+		assert_equal(3, pairs.count)
+	end
+
+	def test_8_names_returns_4_pairs
+		pairs = create_pairs(['Dan','Marvin','Tim','Shirley','Chloe','April','Dan','John'])
+		assert_equal(4, pairs.count)
+	end
+end
+
